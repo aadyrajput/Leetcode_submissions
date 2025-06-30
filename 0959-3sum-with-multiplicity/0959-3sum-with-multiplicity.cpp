23b3e78384911a5a07ab2a0f1793c7559cae3f0e
aadyrajput <aadarshsingh7019@gmail.com>
2025-06-30 16:50:17 +0530

    Time: 0 ms (100%), Space: 14.2 MB (78.57%) - LeetHub

--- 0959-3sum-with-multiplicity/0959-3sum-with-multiplicity.cpp
+++ 0959-3sum-with-multiplicity/0959-3sum-with-multiplicity.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     int threeSumMulti(vector<int>& arr, int target) {
-        int mod = 1e9 + 7;
-        unordered_map<int, int> mpp;
+     int M=1e9+7;
+     unordered_map<int,int> m;
+     for(auto it:arr)m[it]++;
+    long ans=0;
+    for(auto it:m){
+        for(auto itt:m){
+            int a=it.first; int b=itt.first;
+            int c=target-a-b;
+            if(!m.count(c))continue;
 
-        for (int x : arr) mpp[x]++;
-
-        long res = 0;
-        for (auto it1 : mpp) {
-            for (auto it2 : mpp) {
-                int i = it1.first, j = it2.first;
-                int k = target - i - j;
-                if (!mpp.count(k)) continue;
-
-                // i == j == k
-                if (i == j && j == k) {
-                    res += 1L * mpp[i] * (mpp[i] - 1) * (mpp[i] - 2) / 6;
-                }
-                // i == j != k
-                else if (i == j && j != k) {
-                    res += 1L * mpp[i] * (mpp[i] - 1) / 2 * mpp[k];
-                }
-                // i < j < k (avoid duplicates)
-                else if (i < j && j < k) {
-                    res += 1L * mpp[i] * mpp[j] * mpp[k];
-                }
+            if(a==b && b==c){
+                long x=m[a]*1ll*(m[a]-1)*1ll*(m[a]-2)/6;
+                ans+=x;
+                   //   ans += 1L * m[a] * (m[a] - 1) * (m[a] - 2) / 6;
+            }
+            else if(a==b && a!=c){
+                long y=(m[a]*1ll*(m[a]-1))/2; y=y*1ll*m[c];
+                ans+=y;
+            }
+            else if(a<b && b<c){
+                long z=m[a]*1ll*m[b]*1ll*m[c];
+                ans+=z;
             }
         }
-        return res % mod;
+    }
+
+    return ans%M;
     }
 };
