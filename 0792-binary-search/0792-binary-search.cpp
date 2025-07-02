214a9512e64cdddc0280b60ec5a023f4281a6e5f
aadyrajput <aadarshsingh7019@gmail.com>
2025-07-03 01:58:48 +0530

    Time: 0 ms (100%), Space: 31.3 MB (79.79%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -3,13 +3,12 @@ public:
     int search(vector<int>& nums, int target) {
         int n=nums.size();
         int lo=0; int hi=n-1;
-        while(abs(hi-lo)>1){
+        while(lo<=hi){
             int mid=(lo+hi)/2;
-            if(nums[mid]<=target) lo=mid;
-            else hi=mid;
+            if(nums[mid]==target) return mid;
+            else if(target>nums[mid]) lo=mid+1;
+            else hi=mid-1;
         }
-        if(nums[lo]==target)return lo;
-        else if(nums[hi]==target)return hi;
-        else return -1;
+        return -1;
     }
 };
