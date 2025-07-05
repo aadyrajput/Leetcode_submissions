009d4449c9ee9125e0c88ac3d28e0eeb8ac4749b
aadyrajput <aadarshsingh7019@gmail.com>
2025-07-06 02:50:10 +0530

    Time: 278 ms (6.25%), Space: 251.9 MB (6.25%) - LeetHub

--- 3916-minimum-time-to-reach-destination-in-directed-graph/3916-minimum-time-to-reach-destination-in-directed-graph.cpp
+++ 3916-minimum-time-to-reach-destination-in-directed-graph/3916-minimum-time-to-reach-destination-in-directed-graph.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
     int minTime(int n, vector<vector<int>>& edges) {
-        if(n==1){return 0;}
+        if(n == 1)return 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         vector<vector<vector<int>>> adj(n);
-        for(auto it: edges) adj[it[0]].push_back({it[1],it[2],it[3]});
-        vector<int> dist(n,1e9);
-        queue<pair<int,int>> q;
-        q.push({0,0}); 
-        dist[0]=0;
-        while(!q.empty()){
-            int u=q.front().first;
-            int t=q.front().second;
-            q.pop();
-            for(auto it:adj[u]){
-                int v=it[0]; int st=it[1]; int end=it[2];
-                if(dist[u]>=st && dist[u]<=end && dist[v]>dist[u]+1){
-                    dist[v]=dist[u]+1;
-                    q.push({v,dist[v]});
+        for(auto it: edges)
+        {
+            adj[it[0]].push_back({it[1], it[2], it[3]});
+            if(it[0] == 0)
+            {
+                pq.push({it[2], it[1]});
+            }
+        }
+
+        vector<int> vis(n, 0);
+
+        while(!pq.empty())
+        {
+            auto [q, e] = pq.top();
+            pq.pop();
+
+            int xx = q + 1;
+            if(e == n - 1)return xx;
+
+            if(vis[e] == 1)continue;
+            vis[e] = 1;
+
+            for(auto it: adj[e])
+            {
+                int desi = it[0];
+                int lauda1 = it[1];
+                int lauda2 = it[2];
+
+                if(lauda1 <= xx && xx <= lauda2)
+                {
+                    pq.push({xx, desi});
                 }
-                else if(dist[u]<st && dist[v]>st+1){
-                    dist[v]=st+1;
-                    q.push({v,dist[v]});
+                else if(xx < lauda1)
+                {
+                    pq.push({lauda1, desi});
                 }
             }
         }
-    if(dist[n-1]==1e9)return -1;
-    else return dist[n-1];
+        return -1;
     }
 };
