186b7eb06be2576375dbdfc28006e66102a99408
aadyrajput <aadarshsingh7019@gmail.com>
2025-07-20 15:06:38 +0530

    Time: 0 ms (100%), Space: 95.9 MB (18.73%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,68 +1,22 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-
         int m=nums1.size();
         int n=nums2.size();
-        if(m==0 && n==0) return 0;
-        if(n==0){
-            if(m%2!=0) return nums1[m/2];
-            int x=m/2;
-            double ans=nums1[x-1]+nums1[x];
-            ans=ans/2;
-            return ans;
+        int x=(m+n)/2;
+        int i=0; int j=0;
+        vector<int> v;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){ v.push_back(nums1[i]); i++;}
+            else{v.push_back(nums2[j]); j++;}
         }
-        if(m==0){
-                if(n%2!=0) return nums2[n/2];
-            int x=n/2;
-            double ans=nums2[x-1]+nums2[x];
-            ans=ans/2;
-            return ans;
-        }
-        int p1=0; int p2=0; int ct=(m+n)/2;
-        if((m+n)%2==0) ct++;
-        else ct+=2;
-        int ctt=0; int ans1=0; int ans2=0;
+        while(i<m){v.push_back(nums1[i]); i++;}
+        while(j<n){ v.push_back(nums2[j]); j++;}
 
-        while(ctt<ct){
-            ctt++;
-            if(p1<m && p2<n){
-                if(nums1[p1]<nums2[p2]){
-                   if(ctt==ct-1) ans1=nums1[p1]; 
-                   if(ctt==ct) ans2=nums1[p1];
-                   p1++;
-                }   
-                else{
-                    if(ctt==ct-1) ans1=nums2[p2]; 
-                   if(ctt==ct) ans2=nums2[p2];
-                   p2++;
-                }                 
-            }
-        else{
-            if(p1<m){
-                if(ctt==ct-1) ans1=nums1[p1]; 
-                   if(ctt==ct) ans2=nums1[p1];
-                   p1++;
-            }
-            else{
-                  if(ctt==ct-1) ans1=nums2[p2]; 
-                   if(ctt==ct) ans2=nums2[p2];
-                   p2++;
-            }
+        if(v.size()%2!=0) return v[x];
+        double ans=v[x-1]+v[x];
+        ans=ans/2;
+        return ans;
 
-        }
-                
-               
-                
-        }
-        if((m+n)%2!=0){
-            return ans1;
-        }
-        else{
-            double ans=ans1+ans2;
-            ans=ans/2;
-            return ans;
-        }
-          
     }
 };
