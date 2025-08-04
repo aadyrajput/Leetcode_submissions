9c468e0ea6a8e2eaa37d8dfc47bd3743bd5acec0
aadyrajput <aadarshsingh7019@gmail.com>
2025-08-04 22:35:50 +0530

    Time: 0 ms (100%), Space: 69.6 MB (10.16%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -6,13 +6,11 @@ public:
         int zero=0;
         for(int i=0; i<nums.size(); i++){
             if(nums[i]==0) zero++;
-            while(zero>k){
-              if(nums[left]==0){
-                 zero--;
-              }
-              left++;
+            if(zero>k){
+                if(nums[left]==0)zero--;
+                left++;
             }
-            ans=max(ans,i-left+1);
+            if(zero<=k)ans=max(ans,i-left+1);
         }
 
         return ans;
