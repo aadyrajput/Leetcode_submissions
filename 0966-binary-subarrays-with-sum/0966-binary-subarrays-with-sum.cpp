afd151c1f1d7c9c5d6be4b12aff58ea0dd0ad099
aadyrajput <aadarshsingh7019@gmail.com>
2025-08-05 00:28:41 +0530

    Time: 1 ms (61.81%), Space: 32.6 MB (59.42%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,15 +1,22 @@
 class Solution {
+private:
+int fun(vector<int>&nums,int goal){         // number of subaray with sum les than or equal to goal;
+ int ans=0;
+ int sum=0; int left=0;
+ for(int i=0;i<nums.size(); i++){
+    if(goal<0)return 0;
+    sum+=nums[i];
+    while(sum>goal){
+        sum-=nums[left];
+        left++;
+    }
+    ans+=(i-left+1);
+ }
+return ans;
+}
+
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        map<int,int>m;
-        int sum=0;
-        int ans=0;
-        for(int i=0;i<nums.size(); i++){
-            sum+=nums[i];
-            if(sum==goal) ans++;
-            if(m.find(sum-goal)!=m.end())ans+=m[sum-goal];
-            m[sum]++;
-        }
-        return ans;
+     return fun(nums,goal)-fun(nums,goal-1);
     }
 };
