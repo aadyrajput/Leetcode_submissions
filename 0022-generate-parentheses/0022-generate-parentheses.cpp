bc2c1e49400fe0a99306bccd4ebe5303122578bc
aadyrajput <aadarshsingh7019@gmail.com>
2025-08-13 02:36:11 +0530

    Time: 3 ms (71.67%), Space: 15.6 MB (64.87%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,31 +1,22 @@
 class Solution {
 private:
-bool isValid(string s){
-    int ctl=0; int ctr=0;
-    for(int i=0;i<s.size(); i++){
-        if(s[i]=='(')ctl++;
-        else ctr++;
-        if(ctr>ctl)return false;
-    }
-    return ctl==ctr;
-}
-private:
-void fun(int i,int n,string temp,vector<string>&ans){
+void fun(int i,int n,string temp,int open,int close,vector<string>&ans){
     if(i==n*2){
-        if(isValid(temp))ans.push_back(temp);
+        ans.push_back(temp);
         return;
     }
-    temp.push_back('(');
-    fun(i+1,n,temp,ans);
-    temp.pop_back();
-    temp.push_back(')');
-    fun(i+1,n,temp,ans);
+    if(open<n){
+        fun(i+1,n,temp+"(",open+1,close,ans);
+    }
+    if(close<open){
+        fun(i+1,n,temp+")",open,close+1,ans);
+    }
 }
 
 public:
     vector<string> generateParenthesis(int n) {
-        vector<string> ans; string temp;
-        fun(0,n,temp,ans);
+        vector<string> ans; string temp="";
+        fun(0,n,temp,0,0,ans);
         return ans;       
     }
 };
