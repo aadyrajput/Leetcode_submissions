d40dd131c6a127e7eed2c047c8770eca99990799
aadyrajput <aadarshsingh7019@gmail.com>
2025-09-02 23:20:30 +0530

    Time: 18 ms (6.58%), Space: 13.4 MB (12.41%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,15 +9,16 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-     map<ListNode*, bool> vis;
-     ListNode* temp=head;
-     bool flag=false;
-     while(temp!=NULL){
-        if(vis[temp]){flag=true;break;}
-        vis[temp]=true;
-        temp=temp->next;
-     }
-     if(flag) return temp;
-     return NULL;
+        if(head==NULL)return NULL;
+        map<ListNode*,bool> m;
+        ListNode* temp=head;
+        bool flag=false;
+        while(temp){
+            if(m[temp]){flag=true; break;}    
+            m[temp]=true;
+            temp=temp->next; 
+        }
+        if(flag)return temp;
+        return NULL;
     }
 };
