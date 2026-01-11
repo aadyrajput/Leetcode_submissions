56145027524e44141780a8f44481753da5fca5d7
aadyrajput <aadarshsingh7019@gmail.com>
2026-01-11 13:44:07 +0530

    Time: 9 ms (40.3%), Space: 20.8 MB (21.81%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,41 +1,53 @@
 class Solution {
+private:
+int fun(vector<int> &arr){
+stack<int> st; int n=arr.size();
+vector<int> left(n,0);
+for(int i=0; i<n; i++){
+    while(!st.empty() && arr[i]<=arr[st.top()]){
+        st.pop();
+    }
+    if(st.empty())left[i]=0;
+    else left[i]=st.top()+1;
+    st.push(i);
+}
+
+stack<int> st2; 
+vector<int> right(n,0);
+for(int i=n-1; i>=0; i--){
+    while(!st2.empty() && arr[i]<=arr[st2.top()]){
+        st2.pop();
+    }
+    if(st2.empty())right[i]=n-1;
+    else right[i]=st2.top()-1;
+    st2.push(i);
+}
+int ans=INT_MIN;
+for(int i=0; i<n; i++){
+    int x=(right[i]-left[i]+1)*arr[i];
+    ans=max(ans,x);
+}
+return ans;
+}
+
+
+
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int m=matrix.size(); int n=matrix[0].size();
-        vector<vector<int>> arr(m,vector<int>(n,0));
-            for(int i=0;i<m; i++){
-                for(int j=0;j<n; j++){
-                    if(matrix[i][j]=='0')arr[i][j]=0;
-                    else arr[i][j]=1;
-                }
+        
+        int res=INT_MIN;
+        int nn=matrix[0].size();
+        vector<int> arr(nn,0);
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                if(matrix[i][j]=='0') arr[j]=0;
+                else arr[j]+=1;
             }
-    for(int i=1;i<m; i++){
-        for(int j=0;j<n; j++){
-            if(arr[i][j]==0) arr[i][j]=0;
-            else arr[i][j]+=arr[i-1][j];
+            int zz=fun(arr);
+            res=max(res,zz);
         }
-    }
 
-    int area=0;
-    for(int i=0; i<m; i++){
-        stack<int> st;
-        for(int j=0; j<n; j++){
-            while(!st.empty() && arr[i][st.top()]>=arr[i][j]){
-                int ele=arr[i][st.top()]; st.pop();
-                int nse=j;
-                int pse=st.empty() ? -1 : st.top();
-                area=max(area,ele*(nse-pse-1));
-            }
-            st.push(j);
-        }
-        while(!st.empty()){
-            int ele=arr[i][st.top()]; st.pop();
-            int nse=n;
-            int pse=st.empty() ?-1:st.top();
-            area=max(area,ele*(nse-pse-1));
-        }
-    }
 
-return area;
+        return res;
     }
 };
