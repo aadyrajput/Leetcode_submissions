f60a8eeda02e75fb36db74ac04b5cff6f7756a1a
aadyrajput <aadarshsingh7019@gmail.com>
2026-01-23 19:46:04 +0530

    Time: 0 ms (100%), Space: 19.4 MB (73.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,32 +1,62 @@
+class DisjointSet{
+  public:
+  vector<int> rank,par,size;
+  DisjointSet(int n){
+    rank.resize(n+1,0);
+    par.resize(n+1);
+    size.resize(n+1);
+    for(int i=0; i<=n; i++){
+      par[i]=i;
+      size[i]=1;
+    }
+  }
+  int findPar(int node){
+    if(node==par[node]) return node;
+    return par[node]=findPar(par[node]);
+  }
+  int unionByRank(int u,int v){
+    int ultpar_u=findPar(u);
+    int ultpar_v=findPar(v);
+    if(ultpar_u==ultpar_v) return 1;
+    if(rank[ultpar_u]<rank[ultpar_v]) par[ultpar_u]=ultpar_v;
+    else if(rank[ultpar_u]>rank[ultpar_v]) par[ultpar_v]=ultpar_u;
+    else {par[ultpar_v]=ultpar_u; rank[ultpar_u]++;}
+    return 0;
+  }
+   void unionBySize(int u,int v){
+    int ultpar_u=findPar(u);
+    int ultpar_v=findPar(v);
+    if(ultpar_u==ultpar_v) return;
+    if(size[ultpar_u]<size[ultpar_v]) {
+      par[ultpar_u]=ultpar_v;
+      size[ultpar_v]+=size[ultpar_u];
+    }
+    else{
+      par[ultpar_v]=ultpar_u;
+      size[ultpar_u]+=size[ultpar_v];
+    }
+  }
+};
+
+
 class Solution {
 public:
-vector<int> adj[205];
-void fun(int i,vector<int> &vis){
-    vis[i]=1;
-    for(auto it:adj[i]){
-        if(vis[it])continue;
-        fun(it,vis);
-    }
-}
-
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        vector<int> vis(n,0);
         int ans=0;
-        for(int i=0;i<n; i++){
+        DisjointSet ds(n);
+        for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i!=j && isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                if(isConnected[i][j]==1 && i!=j){
+                    ds.unionBySize(i,j);
                 }
-              }
-           }
-        for(int i=0;i<n; i++){
-            if(!vis[i]){
-                fun(i,vis);
-                ans++;
             }
         }
+        for(int i=0;i<n; i++){
+            if(ds.par[i]==i)ans++;
+        }
+
+
     return ans;
     }
 };
