e666b5f1d542e20afced40040e5ab8a091f0e1cd
aadyrajput <aadarshsingh7019@gmail.com>
2026-02-04 16:53:50 +0530

    Time: 14 ms (8.12%), Space: 24.8 MB (12.56%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
         stack<pair<int,int>> st;
-        for(auto it:intervals){
-            int a=it[0]; int b=it[1];
-            if(st.empty()) {st.push({a,b}); continue;}
+        sort(intervals.begin(),intervals.end());
+        st.push({intervals[0][0],intervals[0][1]});
+        for(int i=1;i<intervals.size();i++){
+            int a=intervals[i][0]; int b=intervals[i][1];
             int x=st.top().first; int y=st.top().second;
             if(a>y){
                 st.push({a,b});
             }
             else{
                 st.pop();
-                st.push({x,max(b,y)});
+                st.push({x,max(y,b)});
             }
         }
         vector<vector<int>> ans;
         while(!st.empty()){
-            int a=st.top().first; int b=st.top().second;
-            vector<int> temp; temp.push_back(a); temp.push_back(b);
-            ans.push_back(temp);
+            int a=st.top().first, b=st.top().second;
             st.pop();
+            vector<int> v={a,b};
+            ans.push_back(v);
         }
         reverse(ans.begin(),ans.end());
         return ans;
+
     }
 };
