658d3025cc0b1f6cc94fe8e79a7c241d5d1a1324
aadyrajput <aadarshsingh7019@gmail.com>
2026-02-14 16:32:53 +0530

    Time: 132 ms (17.66%), Space: 74.5 MB (14.36%) - LeetHub

--- 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.cpp
+++ 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.cpp
@@ -1,92 +1,81 @@
 class DisjointSet{
 public:
-    vector<int> par,size;
-    DisjointSet(int n){
-        par.resize(n);
-        size.resize(n,1);
-        for(int i=0;i<n;i++) par[i]=i;
+vector<int> par,size;
+DisjointSet(int n){
+    par.resize(n+1);
+    size.resize(n+1,1);
+    for(int i=0;i<=n;i++)par[i]=i;
+}
+int findPar(int u){
+    if(u==par[u])return u;
+    return par[u]=findPar(par[u]);
+}
+void unionBySize(int u, int v){
+    int up=findPar(u);
+    int vp=findPar(v);
+    if(up==vp)return;
+    if(size[up]<size[vp]){
+        size[vp]+=size[up];
+        par[up]=vp;
     }
-    int findPar(int u){
-        if(u==par[u]) return u;
-        return par[u]=findPar(par[u]);
+    else{
+        size[up]+=size[vp];
+        par[vp]=up;
     }
-    bool unionBySize(int u, int v){
-        int up=findPar(u);
-        int vp=findPar(v);
-        if(up==vp) return false;
-        if(size[up]<size[vp]){
-            size[vp]+=size[up];
-            par[up]=vp;
-        }
-        else{
-            size[up]+=size[vp];
-            par[vp]=up;
-        }
-        return true;
-    }
+}
 };
 
 class Solution {
 public:
-
-    int kruskal(int n, vector<vector<int>>& edges,
-                int skip, int force){
-
-        DisjointSet ds(n);
-        int sum=0;
-        int cnt=0;
-
-        // 🔥 force include edge
-        if(force!=-1){
-            sum+=edges[force][2];
-            ds.unionBySize(edges[force][0],edges[force][1]);
-            cnt++;
-        }
-
-        for(int i=0;i<edges.size();i++){
-            if(i==skip) continue;
-
-            if(ds.unionBySize(edges[i][0],edges[i][1])){
-                sum+=edges[i][2];
-                cnt++;
+    int fun(vector<pair<int,pair<int,int>>>&v,int n,int summ,int forceu,int forcev){
+         DisjointSet ds(n);
+         int ct=0;
+         if(forceu!=-1){
+            ds.unionBySize(forceu,forcev);ct++;
+         }
+        for(auto it:v){
+            int wt=it.first;
+            int u=it.second.first, v=it.second.second;
+            if(ds.findPar(u)!=ds.findPar(v)){
+                summ+=wt; ds.unionBySize(u,v);
+                ct++;
             }
         }
-
-        if(cnt!=n-1) return 1e9;  // not MST
-        return sum;
+        for (int i = 0; i < n; i++) {
+        ds.par[i] = i; 
+        ds.size[i] = 1; 
+        }
+        if(ct!=n-1)return 1e9; 
+        return summ;
     }
 
-    vector<vector<int>> findCriticalAndPseudoCriticalEdges(
-        int n, vector<vector<int>>& edges) {
+    vector<vector<int>> findCriticalAndPseudoCriticalEdges(int n, vector<vector<int>>& edges) {
+        int mst_sum=0; 
+        vector<pair<int,pair<int,int>>> v;
+        for(auto it:edges){
+            v.push_back({it[2],{it[0],it[1]}});
+        }
+        sort(v.begin(),v.end());
+       int summm=0;
+       int minsum=fun(v,n,summm,-1,-1);
 
-        int m=edges.size();
-
-        // attach index
-        for(int i=0;i<m;i++)
-            edges[i].push_back(i);
-
-        sort(edges.begin(),edges.end(),
-            [](auto &a,auto &b){
-                return a[2]<b[2];
-            });
-
-        int minsum=kruskal(n,edges,-1,-1);
-
-        vector<int> critical, pseudo;
-
-        for(int i=0;i<m;i++){
-
-            // 🔥 check critical
-            if(kruskal(n,edges,i,-1)>minsum){
-                critical.push_back(edges[i][3]);
+        vector<int> critical,psuedo;
+        for(int i=0;i<edges.size(); i++){
+            vector<pair<int,pair<int,int>>> v;
+            for(int j=0;j<edges.size(); j++){
+                if(j==i)continue;
+                v.push_back({edges[j][2],{edges[j][0],edges[j][1]}});
             }
-            else{
-                // 🔥 check pseudo
-                if(kruskal(n,edges,-1,i)==minsum)
-                    pseudo.push_back(edges[i][3]);
-            }
+            sort(v.begin(),v.end());
+
+            int summ=0;
+            int z=fun(v,n,summ,-1,-1);
+            if(z==1e9 || z>minsum){critical.push_back(i); continue;}
+            summ=edges[i][2];
+            int zz=fun(v,n,summ,edges[i][0],edges[i][1]);
+            if(zz==minsum)psuedo.push_back(i);
         }
 
-        return {critical,pseudo};
+    return {critical,psuedo};
     }
 };
