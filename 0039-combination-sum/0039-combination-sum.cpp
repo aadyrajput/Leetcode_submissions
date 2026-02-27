eddcf699c2edfca10828382d7aa930b2b42f34ec
aadyrajput <aadarshsingh7019@gmail.com>
2026-02-28 01:54:14 +0530

    Time: 4 ms (38.49%), Space: 14.5 MB (33.02%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void fun(int i,int sum,vector<int>&can,int tar,vector<int>&v,set<vector<int>>&st){
+        if(sum>tar)return;
+        if(i==can.size()){
+            if(sum==tar)st.insert(v);
+            return;
+        }
+        if(sum+can[i]<=tar){
+            v.push_back(can[i]);
+            fun(i,sum+can[i],can,tar,v,st);
+            v.pop_back();
+        }
+        fun(i+1,sum,can,tar,v,st);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        set<vector<int>> st;
+        vector<int> v;
+        fun(0,0,candidates,target,v,st);
+
+        vector<vector<int>> ans;
+        for(auto it:st) ans.push_back(it);
+        return ans;
+    }
+};
