3ead14f6f45a23e08d434cdaa3da4ea39a8572a3
aadyrajput <aadarshsingh7019@gmail.com>
2026-03-03 20:21:44 +0530

    Time: 309 ms (60.91%), Space: 8.9 MB (10%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        solve(board,0,0);
     }
 
 private:
-bool solve(vector<vector<char>>&board){
+bool solve(vector<vector<char>>&board,int x,int y){
+    if(x>=9)return true;
+    if(y>=9)return solve(board,x+1,0);
+    if(board[x][y]!='.') return solve(board,x,y+1);
 
-for(int i=0;i<9;i++){
-    for(int j=0;j<9; j++){
-        if(board[i][j]=='.'){
-            
-            for(char a='1'; a<='9'; a++){
-                if(isValid(i,j,a,board)){
-                    board[i][j]=a;
-
-                    if(solve(board)==true)return true;
-                    else board[i][j]='.';
-                }
-            }
-        return false;
+    for(char a='1'; a<='9'; a++){
+        if(isValid(x,y,a,board)){
+            board[x][y]=a;
+            if(solve(board,x,y+1))return true;
+            board[x][y]='.';
         }
     }
-}
-return true;
+    return false;
 }
 
 bool isValid(int row,int col,char c,vector<vector<char>>&board){
+    for(int i=0;i<9;i++)if(board[i][col]==c)return false;
+    for(int i=0;i<9;i++)if(board[row][i]==c)return false;
 
-for(int i=0;i<9;i++){
-if(board[i][col]==c)return false;
-if(board[row][i]==c)return false;
-if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)]==c)return false;
-}
-return true;
+    int z1=3*(row/3); int z2=3*(col/3);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(board[z1+i][z2+j]==c)return false;
+        }
+    }
+    return true;
 }
 
 
