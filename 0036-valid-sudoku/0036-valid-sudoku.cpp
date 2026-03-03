30173f4a6ee0914f913608bbb3a8d3c59f3e2a0b
aadyrajput <aadarshsingh7019@gmail.com>
2026-03-03 20:28:29 +0530

    Time: 0 ms (100%), Space: 21.4 MB (96.94%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
+    
+bool isValid(int row,int col,char c,vector<vector<char>>&board){
+    if(c=='.')return true;
+    for(int i=0;i<9;i++)if(board[i][col]==c && i!=row)return false;
+    for(int i=0;i<9;i++)if(board[row][i]==c && i!=col)return false;
+
+    int z1=3*(row/3); int z2=3*(col/3);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(board[z1+i][z2+j]==c &&  (z1+i)!=row && (z2+j)!=col)return false;
+        }
+    }
+    return true;
+}
     bool isValidSudoku(vector<vector<char>>& board) {
-        set<char> s;
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-            if(board[i][j]=='.')continue;
-                if(s.find(board[i][j])!=s.end())return false;
-                s.insert(board[i][j]);
-            }
-            s.clear();
-        }
-      
-       for(int j=0; j<9; j++){
-            for(int i=0; i<9; i++){
-                if(board[i][j]=='.')continue;
-                if(s.find(board[i][j])!=s.end())return false;
-                s.insert(board[i][j]);
-            }
-            s.clear();
-        }
-        
-        vector<set<int>> v(9);
-        for(int i=0;i<9; i++){
-            for(int j=0;j<9; j++){
-                if(board[i][j]=='.')continue;
-                int z= (i/3)*3 +(j/3);
-                if(v[z].find(board[i][j]-'0')!=v[z].end())return false;
-                v[z].insert(board[i][j]-'0');
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(!isValid(i,j,board[i][j],board))return false;
             }
         }
     return true;
