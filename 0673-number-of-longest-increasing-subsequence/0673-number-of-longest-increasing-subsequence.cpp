7300402026234157a892c0889683f3d5053a1dea
aadyrajput <aadarshsingh7019@gmail.com>
2026-03-27 13:03:25 +0530

    Time: 33 ms (43.74%), Space: 17.7 MB (10.27%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        vector<int> arr=nums;
-          int n=arr.size();
-        vector<int> dp(n,1),ct(n,1);
-        int maxa=1; 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(arr[j]<arr[i] && 1+dp[j]>dp[i]){
-                    dp[i]=1+dp[j];
-                    ct[i]=ct[j];
-                }        
-                else if(arr[j]<arr[i] && 1+dp[j]==dp[i]){
-                    dp[i]=1+dp[j];
-                    ct[i]+=ct[j];
-                }        
-                
-            }
-            
-        }
-        
-        for(int i=0; i<n; i++){
-            if(dp[i]>maxa){
-                maxa=dp[i];  
-            }
-        }
-    int ctt=0;
-        for(int i=0;i<n;i++){
-            if(dp[i]==maxa){
-                ctt+=ct[i];
-            }
-        }
-    return ctt;
+          // Code here
+        vector<int> arr=nums; int n=arr.size();
+       vector<int> dp(n,1);
+       vector<int> ways(n,1);
+       
+       for(int i=0;i<n;i++){
+           for(int j=0;j<i; j++){
+               if(arr[i]>arr[j] && dp[j]+1>dp[i]){
+                   dp[i]=1+dp[j];
+                   ways[i]=ways[j];
+               }
+              else if(arr[i]>arr[j] && dp[j]+1==dp[i]){
+                   ways[i]+=ways[j];
+               }
+           }
+       }
+       
+       int maxlen=0; int ind=-1;
+       for(int i=0;i<n;i++)maxlen=max(maxlen,dp[i]);
+
+        int ct=0;
+       for(int i=0;i<n;i++){
+        if(dp[i]==maxlen)ct+=ways[i];
+       }
+       return ct;
     }
 };
