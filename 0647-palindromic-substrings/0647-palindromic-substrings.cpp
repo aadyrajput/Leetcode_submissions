68d397cd6ea9b8c375d74d7b3f5a7a3b5d7c1c9d
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-04-17 22:41:06 +0530

    Time: 11 ms (51.92%), Space: 21.3 MB (24.17%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-bool isPalindrom(string &ss){
-   bool ans=true;
-    for(int i=0; i<=ss.size()/2; i++){
-        if(ss[i]!=ss[ss.size()-1-i]) ans=false;
+ int extend(int i,int j,int n,string s){
+    int ans=0;
+    while(i>=0 && j<n && s[i]==s[j]){
+        ans++; i--; j++;
     }
     return ans;
-}
-    int countSubstrings(string s) {
+ }
+  int countSubstrings(string s) {
         int n=s.size();
         int ct=0;
         for(int i=0;i<n;i++){
-            string temp="";
-            for(int j=i;j<n; j++){
-                temp+=s[j];
-                if(isPalindrom(temp))ct++;
-            }
+           ct+=extend(i,i,n,s);
+           ct+=extend(i,i+1,n,s);
         }
         return ct;
 
