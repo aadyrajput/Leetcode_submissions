8ab724c7995c2c43cb7698b48d1674e040ce60f5
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-04-18 14:57:17 +0530

    Time: 0 ms (100%), Space: 8.7 MB (30.08%) - LeetHub

--- 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int mirrorDistance(int n) {
-        int ct=0; int nn=n;
+        int nn=n;
+        int rev=0;
         while(nn){
-            ct++;
+            rev=rev*10;
+            rev+=(nn%10);
             nn/=10;
         }
-        int m=0;
-        ct--; int z=n;
-        while(n){
-            int z=n%10;
-            z=z*pow(10,ct);
-            m+=z;
-            n/=10; ct--;
-        }
-        int ans=abs(z-m);
-        return ans;
-
+        return abs(rev-n);
     }
 };
