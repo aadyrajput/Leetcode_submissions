48afa67af51040748beee56c40fd28ca7a3bf49f
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-04-24 19:26:57 +0530

    Time: 0 ms (100%), Space: 10.7 MB (21.1%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,8 +4,7 @@ int fun(int i,vector<int>&nums,vector<int>&dp){
     if(i==0)return nums[0];
     if(i<0)return 0;
     if(dp[i]!=-1)return dp[i];
-    int a=0;
-    if(i>=1)a=nums[i]+fun(i-2,nums,dp);
+    int a=nums[i]+fun(i-2,nums,dp);
     int b=fun(i-1,nums,dp);
     return dp[i]=max(a,b);
 }
