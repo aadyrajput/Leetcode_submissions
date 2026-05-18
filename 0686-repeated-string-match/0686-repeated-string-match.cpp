b4d2e650f288245e72b62ffdb19daa336d3ba1b4
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-05-19 02:38:44 +0530

    Time: 3 ms (80.27%), Space: 10.3 MB (26.47%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,23 +1,55 @@
+#define ll long long int
 class Solution {
 public:
-    int repeatedStringMatch(string a, string b) {
-        string s = a;
-        int cnt = 1;
+    const ll M1=1e9+7, M2=1e9+33;
+    const ll rad1=26, rad2=27;
 
-        while (s.size() < b.size()) {
-            s += a;
-            cnt++;
+    pair<ll, ll> has(string x,int m){
+        long long ans1=0, ans2=0,fact1=1, fact2=1;
+        for(int i=m-1; i>=0; i--){
+            ans1+=((x[i]-'a')*1ll*fact1)%M1;
+            ans2+=((x[i]-'a')*1ll*fact2)%M2;
+            fact1=(fact1*1ll*rad1)%M1;
+            fact2=(fact2*1ll*rad2)%M2;
         }
+    return {ans1%M1,ans2%M2};
+    }
+    
+    bool fun(string &a,string &b){
+        if(a==b)return true;
+        ll n=a.size(), m=b.size();
+        ll maxa1=1, maxa2=1;
+        for(int i=0;i<m; i++){
+            maxa1=(maxa1*1ll*rad1)%M1;
+            maxa2=(maxa2*1ll*rad2)%M2;
+        }
+        pair<ll,ll> hasb=has(b,m);
+        pair<ll,ll> hasa={0,0};
 
-        if (s.find(b) != string::npos)
-            return cnt;
+        for(int i=0;i<=n-m;i++){
+            if(i==0){
+                hasa=has(a,m);
+            }
+            else{
+        hasa.first=((hasa.first*rad1)%M1-((a[i-1]-'a')*maxa1)%M1+(a[m-1+i]-'a')+M1)%M1;
+        hasa.second=((hasa.second*rad2)%M2-((a[i-1]-'a')*maxa2)%M2+(a[m-1+i]-'a')+M2)%M2;
+            }
+            if(hasa.first==hasb.first && hasa.second==hasb.second)return true;
+        }
+    return false;
+    }
 
-        s += a;
-        cnt++;
-
-        if (s.find(b) != string::npos)
-            return cnt;
-
+    int repeatedStringMatch(string a, string b) {
+        if(a==b) return 1;
+        int ct=1;
+        string aa=a;
+        while(aa.size()<b.size()){
+            aa+=a;
+            ct++;
+        }
+        if(fun(aa,b))return ct;
+        aa+=a; ct++;
+        if(fun(aa,b))return ct;
         return -1;
     }
 };
