28c893f0204e90b7747d6614e506899b05f168fe
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-06-04 21:10:30 +0530

    Time: 8 ms (13.06%), Space: 9.6 MB (32.79%) - LeetHub

--- 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.cpp
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.cpp
@@ -1,62 +1,52 @@
-#include <functional>
-#include <semaphore>
-using namespace std;
-
 class FizzBuzz {
-private: 
+private:
     int n;
- counting_semaphore<1> semNumber{1};
-    counting_semaphore<1> semFizz{0};
-    counting_semaphore<1> semBuzz{0};
-    counting_semaphore<1> semFizzBuzz{0};
-
+    mutex m;
+    condition_variable c;
+    int i;
 public:
     FizzBuzz(int n) {
         this->n = n;
+        this->i=1;
     }
 
     // printFizz() outputs "fizz".
     void fizz(function<void()> printFizz) {
-        
-        for(int i=3;i<=n; i+=3){
-            if(i%5==0)continue;
-            semFizz.acquire();
-            printFizz();
-            semNumber.release();
+        while(i<=n){
+        unique_lock<mutex> lock(m);
+        while(i<=n && ((i%3==0) && (i%5!=0))==0) c.wait(lock);
+            if(i<=n){printFizz(); i++;}
+            c.notify_all();
         }
     }
 
     // printBuzz() outputs "buzz".
     void buzz(function<void()> printBuzz) {
-
-          for(int i=5;i<=n; i+=5){
-            if(i%3==0)continue;
-            semBuzz.acquire();
-            printBuzz();
-            semNumber.release();
+        while(i<=n){
+          unique_lock<mutex> lock(m);
+        while(i<=n && ((i%5==0) && (i%3!=0))==0)c.wait(lock);
+           if(i<=n){printBuzz(); i++;}
+            c.notify_all();
         }
     }
 
     // printFizzBuzz() outputs "fizzbuzz".
 	void fizzbuzz(function<void()> printFizzBuzz) {
-         for(int i=15;i<=n; i+=15){
-            semFizzBuzz.acquire();
-            printFizzBuzz();
-            semNumber.release();
+        while(i<=n){
+          unique_lock<mutex> lock(m);
+        while(i<=n && ((i%3==0) && (i%5==0))==0)c.wait(lock);
+           if(i<=n){printFizzBuzz(); i++;}
+            c.notify_all();
         }
     }
 
     // printNumber(x) outputs "x", where x is an integer.
     void number(function<void(int)> printNumber) {
-        for(int i=1;i<=n; i++){
-            semNumber.acquire();
-            if(i%3==0 && i%5==0) semFizzBuzz.release();
-            else if(i%3==0)semFizz.release();
-            else if(i%5==0)semBuzz.release();
-            else{
-                printNumber(i);
-                semNumber.release();
-            }
+        while(i<=n){
+          unique_lock<mutex> lock(m);
+        while(i<=n && ((i%3!=0) && (i%5!=0))==0)c.wait(lock);
+           if(i<=n){printNumber(i); i++;}
+            c.notify_all();
         }
     }
 };
