de4cde9e8bd86d762fa619a5374c22137d580cec
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-06-11 02:37:06 +0530

    Time: 0 ms (100%), Space: 9 MB (37.44%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-     stack<char> st;
-     map<char,int>m;
-     m['(']=-1; m[')']=1; m['{']=-2; m['}']=2;m['[']=-3; m[']']=3;
-     
-    for(int i=0;i<s.size(); i++){
-        int x=m[s[i]];
-        if(x<0){st.push(x); continue;}
-        int xx=(-1*x);
-        if(st.empty()) return false;
-        if(st.top()==xx){
-            st.pop();
+        stack<char> st;
+        int n=s.size();
+        map<char,int> m={{'(',-1},{')',1},{'[',-2},{']',2},{'{',-3},{'}',3}};
+
+        for(int i=0;i<n;i++){
+            if(st.empty()){
+                st.push(s[i]);
+                continue;
+            }
+            int z=m[st.top()];
+            int zz=m[s[i]];
+            if(z+zz==0 && z<0 && zz>0)st.pop();
+            else st.push(s[i]);
         }
-        else st.push(x);
-     }
-     if(st.empty()) return true;
+
+        if(st.empty())return true;
         return false;
     }
 };
