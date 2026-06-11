8c33605a2b9f0d64ba8f23277491dd891ab6a3fd
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-06-12 02:20:53 +0530

    Time: 395 ms (5.04%), Space: 216.5 MB (5.1%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        multiset<int> m;  int n=nums.size();
-        for(int i=0;i<k; i++){
+        int n=nums.size();
+        multiset<int> m;
+        for(int i=0;i<k;i++){
             m.insert(nums[i]);
         }
+        int maxa=*(--m.end());
         vector<int> ans;
-        auto it=m.end(); it--;
-        ans.push_back(*it);
-        int p1=0;
-        for(int i=k; i<n; i++){
-            m.erase(m.find(nums[p1]));
+        ans.push_back(maxa);
+        for(int i=k; i<n;i++){
+            m.erase(m.find(nums[i-k]));
             m.insert(nums[i]);
-            auto itt=m.end(); itt--;
-            ans.push_back(*itt);
-            p1++;
+            ans.push_back(*(--m.end()));
         }
     return ans;
     }
