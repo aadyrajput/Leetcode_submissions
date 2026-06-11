82141fde063a60a10662cb17f902c3fbb2f109d3
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-06-11 17:55:08 +0530

    Time: 3 ms (40.86%), Space: 13 MB (14.44%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums2.size();
-        map<int,int> nge;
-        stack<int> st;
-        for(int i=0;i<nums2.size(); i++){
-            while(!st.empty() && nums2[i]>st.top()){
-                nge[st.top()]=nums2[i];
-                st.pop();
-            }
-            st.push(nums2[i]);
-        }
-        while(!st.empty()){
-            nge[st.top()]=-1; st.pop();
-        }
-        vector<int> ans;
-        for(int i=0;i<nums1.size(); i++){
-            ans.push_back(nge[nums1[i]]);
-        }
-        return ans;
-    
+     int n1=nums1.size(), n2=nums2.size();
+     stack<int> st;
+    map<int,int> m;
 
+    st.push(nums2[0]);
+    for(int i=0;i<n2;i++){
+        while(!st.empty() && nums2[i]>st.top()){
+            m[st.top()]=nums2[i];
+            st.pop();
+        }
+        st.push(nums2[i]);
+    }
+    while(!st.empty()){
+        m[st.top()]=-1;
+        st.pop();
+    }
 
+    vector<int> ans;
+    for(auto it:nums1){
+        ans.push_back(m[it]);
+    }
 
+    return ans;
     }
 };
