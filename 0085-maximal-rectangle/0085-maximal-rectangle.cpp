dfe23e10259e10b6fc46f19e72511bc57ef2221c
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-06-12 01:22:28 +0530

    Time: 11 ms (39.94%), Space: 23.1 MB (14.41%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,53 +1,64 @@
 class Solution {
-private:
-int fun(vector<int> &arr){
-stack<int> st; int n=arr.size();
-vector<int> left(n,0);
-for(int i=0; i<n; i++){
-    while(!st.empty() && arr[i]<=arr[st.top()]){
-        st.pop();
-    }
-    if(st.empty())left[i]=0;
-    else left[i]=st.top()+1;
-    st.push(i);
-}
-
-stack<int> st2; 
-vector<int> right(n,0);
-for(int i=n-1; i>=0; i--){
-    while(!st2.empty() && arr[i]<=arr[st2.top()]){
-        st2.pop();
-    }
-    if(st2.empty())right[i]=n-1;
-    else right[i]=st2.top()-1;
-    st2.push(i);
-}
-int ans=INT_MIN;
-for(int i=0; i<n; i++){
-    int x=(right[i]-left[i]+1)*arr[i];
-    ans=max(ans,x);
-}
-return ans;
-}
-
-
-
 public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        
-        int res=INT_MIN;
-        int nn=matrix[0].size();
-        vector<int> arr(nn,0);
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                if(matrix[i][j]=='0') arr[j]=0;
-                else arr[j]+=1;
+     vector<int> next_smaller(vector<int> &arr){
+        int n=arr.size();
+        vector<int> ans(n,-1);
+        stack<int> st;
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
+                st.pop();
             }
-            int zz=fun(arr);
-            res=max(res,zz);
+            if(st.empty())ans[i]=n;
+            else ans[i]=st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+
+    vector<int> prev_smaller(vector<int> &arr){     
+        int n=arr.size();
+        vector<int> ans(n,-1);
+        stack<int> st;
+        for(int i=0; i<n; i++){
+            while(!st.empty() && arr[st.top()]>arr[i]){
+                st.pop();
+            }
+            if(st.empty())ans[i]=-1;
+            else ans[i]=st.top();
+            st.push(i);
+        }
+        return ans;
+     }
+
+    
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        vector<int> temp(cols,0);
+
+        for(int i=0;i<cols;i++){
+            temp[i]=matrix[0][i]-'0';
+        }
+        vector<int> prev=prev_smaller(temp);
+        vector<int> next=next_smaller(temp);
+        long long ans=0;
+        for(int i=0;i<cols;i++){
+            int z=next[i]-prev[i]-1;
+            ans=max(ans,z*1ll*temp[i]);
         }
 
-
-        return res;
+        for(int i=1;i<rows; i++){
+            for(int j=0;j<cols;j++){
+                if(matrix[i][j]=='0')temp[j]=0;
+                else temp[j]++;
+            }
+            vector<int> prev=prev_smaller(temp);
+            vector<int> next=next_smaller(temp);
+            for(int k=0;k<cols;k++){
+                int z=next[k]-prev[k]-1;
+                ans=max(ans,z*1ll*temp[k]);
+            }
+        }
+    return ans;
     }
 };
