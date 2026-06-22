4f0d8749447d23ab36032b7bca32359665540a40
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-06-22 21:47:56 +0530

    Time: 748 ms (8.08%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT employee_id,department_id FROM Employee
+WHERE primary_flag='Y' OR
+employee_id IN (
+    SELECT employee_id FROM Employee group by employee_id having count(employee_id)=1
+);
