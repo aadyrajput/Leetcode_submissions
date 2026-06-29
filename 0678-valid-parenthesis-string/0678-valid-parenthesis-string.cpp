0f838d9f1f9ff5d591b567afd94a632bdcbf117e
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-06-30 03:02:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,29 +1,22 @@
 class Solution {
-private:
-int fun(int i,int open,string &s, vector<vector<int>>&dp){
-        if(i==s.size()){
-            if(open==0) return true;
-            return false;
-        }
-        if(dp[i][open]!=-1)return dp[i][open];
-        bool flag=false;
-        if(s[i]=='*'){
-            flag= flag | fun(i+1,open+1,s,dp);
-            if(open!=0) flag= flag | fun(i+1,open-1,s,dp);
-            flag= flag | fun(i+1,open,s,dp);
-        }
-        else{
-            if(s[i]=='(') flag= fun(i+1,open+1,s,dp);
-            else if(open!=0){
-                flag= fun(i+1,open-1,s,dp);
-            }
-        }
-        return dp[i][open]=flag;
-}
 public:
     bool checkValidString(string s) {
-        int n=s.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return fun(0,0,s,dp);
+         int mina=0;
+         int maxa=0;
+         for(auto it:s){
+            if(it=='('){
+                mina++; maxa++;
+            }
+            else if(it==')'){
+                mina--; maxa--;
+            }
+            else{
+                mina--; maxa++;
+            }
+            
+            if(mina<0)mina=0;
+         }
+         if(mina==0)return true;
+         return false;
     }
 };
