8c63b233475b038bc38163e5bdfc96e0e2369fbd
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-06-29 16:10:18 +0530

    Time: 2683 ms (5.01%), Space: 55.9 MB (6.44%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,10 +3,11 @@ public:
     bool fun(int i,vector<int>&nums,vector<int>&dp,int n){
         if(i>=n-1)return true;
         if(dp[i]!=-1)return dp[i];
+        bool ans=false;
         for(int j=i+1; j<=min(n-1,i+nums[i]); j++){
-            if(fun(j,nums,dp,n)) return dp[i]=true;
+            ans=ans|fun(j,nums,dp,n);
         }
-        return dp[i]=false;
+        return dp[i]=ans;
     }
     bool canJump(vector<int>& nums) {
         int n=nums.size();
