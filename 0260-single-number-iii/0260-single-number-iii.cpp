6307c7c072be784e58e0c0dd4f810330561ce8e3
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-07-05 11:44:00 +0530

    Time: 0 ms (100%), Space: 13.8 MB (97.27%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int a,b; bool flag=false; int n=nums.size();
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1]){
-                i++; continue;
-            }
-            else{
-                if(!flag){a=nums[i]; flag=true;}
-                else{b=nums[i];}
+        int xorr=0;
+        for(auto it:nums)xorr=xorr^it;
+        int bit=0;
+        for(int i=0;i<=32; i++){
+            if(xorr&(1LL<<i)){
+                bit=i; break;
             }
         }
-        if(nums[n-1]!=nums[n-2])b=nums[n-1];
-        return {a,b};
+
+        int num1=0, num2=0;
+        for(auto it:nums){
+            if(it&(1<<bit))num1=num1^it;
+            else num2=num2^it;
+        }
+
+        return {num1,num2};
     }
 };
