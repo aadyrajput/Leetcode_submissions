1d58ae2a9dd18d4fa8112b47867ec763e0733b95
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-07-05 18:52:30 +0530

    Time: 226 ms (6.88%), Space: 28.6 MB (15.15%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.cpp
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         int ct=0;
-        map<int,int> m1,m2;
+        map<int,int> m1;
         for(int i=0;i<nums1.size();i++){
             for(int j=0;j<nums2.size();j++){
                     m1[nums1[i]+nums2[j]]++;
@@ -10,14 +10,10 @@ public:
         }
           for(int i=0;i<nums3.size();i++){
             for(int j=0;j<nums4.size();j++){
-                    m2[nums3[i]+nums4[j]]++;
+                    ct+=m1[-nums3[i]-nums4[j]];
             }
         }
 
-        for(auto it:m1){
-            if(m2.count(-it.first)==0)continue;
-            ct+=(m2[-it.first]*it.second);
-        }
     return ct;
     }
 };
