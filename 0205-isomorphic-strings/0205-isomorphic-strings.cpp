3c18bbacd6b77bccbd3598ffc4df48444820bd3c
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-07-08 13:15:37 +0530

    Time: 2 ms (49.02%), Space: 9.2 MB (94.76%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         map<char,char> m;
-        for(int i=0; i<s.size(); i++){
+        int n=s.size();
+        for(int i=0;i<n;i++){
             if(m.find(s[i])==m.end()){
                 for(auto it:m){
                     if(it.second==t[i])return false;
-                }
-               m[s[i]]=t[i]; 
+                }   
+                m[s[i]]=t[i];
             }
             else{
-                if(m[s[i]]!=t[i]) return false;
+                if(m[s[i]]!=t[i])return false;
             }
         }
         return true;
-
     }
 };
