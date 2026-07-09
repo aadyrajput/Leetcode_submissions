4b030b4ee29a3963c41658e15228140185d3b51d
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-07-09 17:45:33 +0530

    Time: 0 ms (100%), Space: 9 MB (92.31%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,45 +1,40 @@
 class Solution {
 public:
-    void fun(string &pat, vector<int>&lps, int m){
-        int len=0;  // length of prev pref=suf;
-        
-        lps[0]=0;
-        int i=1;
-        
-        while(i<m){
-            if(pat[i]==pat[len]){
-                len++;
-                lps[i]=len;
-                i++;
-            }
-            else{
-                if(len!=0)len=lps[len-1];
-                else{
-                    lps[i]=0; i++;
-                }
-            }
-        }   
+    int M=1e9+7;
+    int radii=26;
+
+    int fun(string &s, int m){
+        int mult=1;
+        int ans=0;
+        for(int i=m-1;i>=0; i--){
+            ans=(ans+ ((s[i]-'a')*1ll*mult)%M )%M;
+            mult=(mult*1ll*radii)%M;
+        }
+    return ans;
     }
+
     int strStr(string haystack, string needle) {
         int n=haystack.size();
         int m=needle.size();
-        
-        vector<int> lps(m,0);
-        fun(needle,lps,m);
-        
-        int i=0, j=0;
-        while(i<n && j<m){
-            if(haystack[i]==needle[j]){
-                i++; j++;
-            }
-            if(j==m){
-                return i-j;
-            }
-            else if(haystack[i]!=needle[j]){
-                if(j!=0) j=lps[j-1];
-                else i++;
-            }
+
+        int hasN=fun(needle,m);
+
+        int maxa=1;
+        for(int i=0;i<m;i++){
+            maxa=(maxa*1ll*radii)%M;
         }
-        return -1;   
+
+        int hasH=0;
+        for(int i=0;i<=n-m;i++){
+            if(i==0){
+                hasH=fun(haystack,m);
+            }
+            else{
+                hasH=((hasH*1ll*radii)%M -((haystack[i-1]-'a')*1ll*maxa)%M + (haystack[m+i-1]-'a')%M  + M)%M;
+            }
+            if(hasH==hasN)return i;
+        }
+
+    return -1;
     }
 };
