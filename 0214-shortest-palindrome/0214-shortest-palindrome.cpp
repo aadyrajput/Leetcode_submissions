c9fdf2469835eb2a27f0de7cabe9a3562f1d54f0
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-07-09 19:02:40 +0530

    Time: 0 ms (100%), Space: 15.8 MB (42.57%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,15 +1,39 @@
 class Solution {
 public:
-    string shortestPalindrome(string s) {
-        int n=s.size();
-        string rev=s;
-        reverse(rev.begin(),rev.end());
-                                                                    // O(n^2)
-        for(int i=0; i<n; i++){
-            if(!memcmp(s.c_str(),rev.c_str()+i,n-i)){
-                return rev.substr(0,i)+s;
+    void fun(string &pat, vector<int>&lps, int m){
+        int len=0;  // length of prev pref=suf;
+        
+        lps[0]=0;
+        int i=1;
+        
+        while(i<m){
+            if(pat[i]==pat[len]){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len!=0)len=lps[len-1];
+                else{
+                    lps[i]=0; i++;
+                }
             }
         }
-    return rev+s;
+    }
+        
+    string shortestPalindrome(string s) {
+        
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        
+        string neww=s+'-'+rev;
+        int zz=neww.size();
+        vector<int> lps(zz,0);
+        fun(neww,lps,zz);
+
+        int len=lps[zz-1];
+
+        string culprit=rev.substr(0,s.size()-len);
+        return culprit + s;
     }
 };
