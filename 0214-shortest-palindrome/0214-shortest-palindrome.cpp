2fc141c17fa52ec4bfb6df9f2db1002ececeb11a
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-07-09 18:38:11 +0530

    Time: 17 ms (6.46%), Space: 12.4 MB (86.09%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -4,7 +4,7 @@ public:
         int n=s.size();
         string rev=s;
         reverse(rev.begin(),rev.end());
-
+                                                                    // O(n^2)
         for(int i=0; i<n; i++){
             if(!memcmp(s.c_str(),rev.c_str()+i,n-i)){
                 return rev.substr(0,i)+s;
