40599dbe337490f372b9b153701924e83e61d64b
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-07-11 03:00:30 +0530

    Time: 0 ms (100%), Space: 77.2 MB (45.58%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,53 +11,28 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        string s1,s2;
         ListNode* temp1=l1;
-        while(temp1){
-            char c=temp1->val+'0';
-            s1.push_back(c);
-            temp1=temp1->next;
-        }
-         ListNode* temp2=l2;
-        while(temp2){
-             char c=temp2->val+'0';
-            s2.push_back(c);
-            temp2=temp2->next;
-        }
-        string ans;
-        int m=s1.size(); int n=s2.size();
-        if(m<n){
-            int z=n-m;
-            while(z--)s1.push_back('0');
-        }
-        else if(m>n){
-            int z=m-n;
-            while(z--)s2.push_back('0');
-        }
+        ListNode* temp2=l2;
+        int car=0;
 
-        reverse(s1.begin(),s1.end());
-        reverse(s2.begin(),s2.end());
-        int carry=0; int nn=s1.size();
-        for(int i=nn-1; i>=0; i--){
-            int a=s1[i]-'0'; int b=s2[i]-'0';
-            int c=a+b+carry;
-            if(c<=9){string aa=to_string(c); ans+=aa; carry=0;}
-            else{int cc=c%10; carry=c/10; string aa=to_string(cc); ans+=aa;}
-        }
-        if(carry>0){
-            string aa=to_string(carry);
-            ans+=aa;
-        }
-        //reverse(ans.begin(),ans.end());
-        ListNode* head=new ListNode(ans[0]-'0');
-        ListNode* temp=head;
-        for(int i=1; i<ans.size(); i++){
-            ListNode* tt=new ListNode(ans[i]-'0');
-            temp->next=tt;
-            temp=temp->next;
-        }
-        return head;
+        ListNode* ans=new ListNode(-1);
+        ListNode* dum=ans;
 
-
+        while(temp1!=NULL || temp2!=NULL){
+            int z=0;
+            if(temp1){z+=temp1->val; temp1=temp1->next;}
+            if(temp2){z+=temp2->val; temp2=temp2->next;}
+            z+=car;
+            ListNode* x=new ListNode(z%10);
+            car=(z/10);
+             dum->next=x;
+             dum=dum->next;
+        }
+        if(car==1){
+            ListNode* x=new ListNode(car);
+            dum->next=x;
+        }
+    
+    return ans->next; 
     }
 };
