45883fdfdaac05e4b8debd70a1d966fd595a14dd
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-07-15 14:49:29 +0530

    Time: 839 ms (17.96%), Space: 12 MB (8.89%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    int m,n,z;
-    int delrow[4]={-1,0,1,0};
-    int delcol[4]={0,-1,0,1};
+    int dr[4]={-1,0,1,0};
+    int dc[4]={0,-1,0,1};
 
-    bool fun(int i,int j,int z,vector<vector<char>>& board, string word){
+    bool fun(int i,int j,int z,string word,vector<vector<char>>&board,vector<vector<int>>&vis){
         if(z==word.size()){
             return true;
-        }
-        if(i<0 || i>=m || j<0 || j>=n || word[z]!=board[i][j])return false;
+        } 
+    if(i<0 || i>=board.size() || j<0 || j>=board[0].size()|| board[i][j]!=word[z] || vis[i][j]==1)return false;
 
-        char x=board[i][j];
-        board[i][j]='#';
-        for(int aa=0;aa<4; aa++){
-            if(fun(i+delrow[aa],j+delcol[aa],z+1,board,word))return true;
+        vis[i][j]=1;
+        for(int k=0;k<4;k++){
+            int ni=i+dr[k];
+            int nj=j+dc[k];
+            if(fun(ni,nj,z+1,word,board,vis)) return true;
         }
-        board[i][j]=x;
+        vis[i][j]=0;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        m=board.size();
-        n=board[0].size();
-        z=word.size();
+        int m=board.size();
+        int n=board[0].size();
+
+        vector<vector<int>> vis(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]==word[0]){
-                    if(fun(i,j,0,board,word))return true;
+                    int z=0;
+                    if(fun(i,j,z,word,board,vis))return true;
                 }
             }
         }
-
-        return false;
+    return false;
     }
 };
