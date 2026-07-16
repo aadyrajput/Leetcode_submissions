f2a9c419aab8e71141397e6c20db66e3c2dda249
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-07-16 14:59:33 +0530

    Time: 4 ms (22.25%), Space: 14.3 MB (29.02%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    void fun(int i,int tar,vector<int>&can,vector<int>&v,set<vector<int>>&st){
-        if(tar==0 || i==can.size()){
-            if(tar==0)st.insert(v);
-            return;
-        }
-        for(int j=i; j<can.size(); j++){
-            if(j>i && can[j]==can[j-1])continue;
-            if(can[j]>tar)break;
-            v.push_back(can[j]);
-            fun(j+1,tar-can[j],can,v,st);
-            v.pop_back();    
-        }
-    }
+    set<vector<int>> ans;
+    void fun(int i,int tar,vector<int>&arr,vector<int>&temp){
+      if(tar==0){
+        ans.insert(temp);
+        return;
+      }
+      if(i==arr.size())return;
 
+      for(int j=i; j<arr.size();j++){
+         if(j>i && arr[j]==arr[j-1]) continue;
+         if(arr[j]>tar)continue;
+         temp.push_back(arr[j]);
+         fun(j+1,tar-arr[j],arr,temp);
+         temp.pop_back();
+      }
+  }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
-        set<vector<int>> st;
-        vector<int> v;
-        fun(0,target,candidates,v,st);
+         int n=candidates.size();
+        vector<int> temp;
+        fun(0,target,candidates,temp);
 
-        vector<vector<int>> ans;
-        for(auto it:st) ans.push_back(it);
-        return ans;
+        vector<vector<int>> res;
+        for(auto it:ans)res.push_back(it);
+        return res;
     }
 };
