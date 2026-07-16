7e3414dcc60635ad0095abc75544465179ffc733
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-07-16 14:34:58 +0530

    Time: 0 ms (100%), Space: 14.3 MB (39.92%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    void fun(int i,int sum,vector<int>&can,int tar,vector<int>&v,set<vector<int>>&st){
-        if(i==can.size()){
-            if(sum==tar){
-                st.insert(v);
-            }
-            return;
-        }
-        if(sum+can[i]<=tar){
-            v.push_back(can[i]);
-            fun(i,sum+can[i],can,tar,v,st);
-            v.pop_back();
-        }
-        fun(i+1,sum,can,tar,v,st);
-    }
+    vector<vector<int>> ans;
+    void fun(int i,int tar,vector<int>&arr,vector<int>&temp){
+      if(tar==0){
+        ans.push_back(temp);
+        return;
+      }
+      if(i<0)return;
 
+      if(arr[i]<=tar){
+        temp.push_back(arr[i]);
+        fun(i,tar-arr[i],arr,temp);
+        temp.pop_back();
+      }
+      fun(i-1,tar,arr,temp);
+  }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        set<vector<int>> st;
-        vector<int> v;
-        fun(0,0,candidates,target,v,st);
-
-        vector<vector<int>> ans;
-        for(auto it:st) ans.push_back(it);
+         int n=candidates.size();
+        vector<int> temp;
+        fun(n-1,target,candidates,temp);
         return ans;
     }
 };
