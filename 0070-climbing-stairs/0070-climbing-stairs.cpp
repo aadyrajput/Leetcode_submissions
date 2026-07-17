0ffabee5d31826fbbc57b017400175cd78bb06cf
Aadarsh Singh <aadarshsingh7019@gmail.com>
2026-07-17 18:38:20 +0530

    Time: 0 ms (100%), Space: 7.9 MB (58.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int dp[50];
-    int fun(int x){
-        if(x<=2)return x;
-        if(dp[x]!=-1)return dp[x];
-        return dp[x]=fun(x-1)+fun(x-2);
-    }
     int climbStairs(int n) {
         if(n<=2)return n;
         memset(dp,-1,sizeof(dp));
-        return fun(n);
+        
+        dp[0]=1; dp[1]=1;
+        for(int i=2;i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
